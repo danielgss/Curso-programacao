4a75cce060d534caae57b5aed2d0f63fbd99ce52
gabriel160499@gmail.com <gabriel160499@gmail.com>
2021-06-22 14:08:32 -0300

    TemplateStrings

--- FundametosJS/templateString.js
+++ FundametosJS/templateString.js
@@ -4,3 +4,9 @@ const template = `
     Olá
     ${nome}!`
 console.log(concatenacao, template)
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`) 
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei.. ${up('cuidado')}!`)
